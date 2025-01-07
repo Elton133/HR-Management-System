ff7954c02712358c36241bdb5096a8d9724178f8
Elton133 <134627073+Elton133@users.noreply.github.com>
2025-01-07 21:01:36 +0000

    Adjustments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='flex items-center justify-center h-10'>
+    <footer className='flex items-center justify-center h-10 text-[12px]'>
         <aside>
     <p>Copyright © {new Date().getFullYear()} - All right reserved by TeamVault</p>
   </aside>
